c623d4480733dc21a5e12a11f3837b73d70c5d1b
Kimberly Prijadi <prijadikimberly@gmail.com>
2025-03-30 16:16:20 -0400

    some fixes

--- final_ver/user_input_form.py
+++ final_ver/user_input_form.py
@@ -4,6 +4,7 @@ CSC111 Project 2 User Input Form
 Form to gather information from user about an ideal laptop and display recommendations
 """
 from random import randint
+from typing import Optional
 
 import pygame
 from graph_class import load_laptop_graph, add_dummy
@@ -11,22 +12,23 @@ import requests
 from io import BytesIO
 import doctest
 import python_ta
-import python_ta.contracts as contracts
+
+# import python_ta.contracts as contracts
 
 pygame.init()
 
-clock = pygame.time.Clock()
-screen = pygame.display.set_mode([1600, 900])
-font = pygame.font.Font(None, 20)
+CLOCK = pygame.time.Clock()
+SCREEN = pygame.display.set_mode([1600, 900])
+FONT = pygame.font.Font(None, 20)
 
-color_active = pygame.color.Color(179, 158, 181)
-color_inactive = pygame.color.Color(80, 80, 80)
-white = (255, 255, 255)
-bg_color = (30, 30, 30)
+COLOR_ACTIVE = pygame.color.Color(179, 158, 181)
+COLOR_INACTIVE = pygame.color.Color(80, 80, 80)
+WHITE = (255, 255, 255)
+BG_COLOR = (30, 30, 30)
 
 DUMMY_LAPTOP_ID = -1
 
-questions = [
+QUESTIONS = [
     'What is the minimum price you would pay for a laptop(CAD)? (*)',
     'What is the maximum price you would pay for a laptop (CAD)? (*)',
     'Which processor would you like to have? (Intel/AMD/Apple)',
@@ -38,7 +40,7 @@ questions = [
     'How many laptop recommendations would you like? (Integer from 1-20) (*)'
 ]
 
-valid_options = [
+VALID_OPTIONS = [
     None,
     None,  # can be any price
     ['Intel', 'AMD', 'Apple'],
@@ -50,33 +52,40 @@ valid_options = [
     [str(i) for i in range(1, 21)]
 ]
 
-box_width = 200
-box_height = 50
-box_spacing = 100
+BOX_WIDTH = 200
+BOX_HEIGHT = 50
+BOX_SPACING = 100
 
 
 class InputBox:
     """Create an Input Box to gather user input. Takes in the x and y positions, w (width), and h (height).
     It also takes questions from the list created and only allows the user to submit their answers if their answers
     are in valid options."""
+    active: bool
+    rect: pygame.Rect
+    color: pygame.color.Color
+    text: str
+    question: str
+    valid_options: Optional[list[str]] = None
 
-    def __init__(self, x, y, w, h, question, valid_opt):
+    def __init__(self, dim: tuple[int, int, int, int], question: str, valid_opt: Optional[list[str]] = None) -> None:
         self.active = False
+        x, y, w, h = dim
         self.rect = pygame.Rect(x, y, w, h)
-        self.color = color_inactive
+        self.color = COLOR_INACTIVE
         self.text = ''
         self.question = question
         self.valid_options = valid_opt
 
-    def event_handler(self, event):
+    def event_handler(self, event: pygame.event) -> None:
         """Handles the events occurring in the main loop of the form display."""
 
         if event.type == pygame.MOUSEBUTTONDOWN:
             self.active = self.rect.collidepoint(event.pos)
             if self.active:
-                self.color = color_active
+                self.color = COLOR_ACTIVE
             else:
-                self.color = color_inactive
+                self.color = COLOR_INACTIVE
 
         if self.active:
             if event.type == pygame.KEYDOWN:
@@ -85,16 +94,16 @@ class InputBox:
                 else:
                     self.text += event.unicode
 
-    def draw_box(self):
+    def draw_box(self) -> None:
         """Draws the input box and text."""
-        question_surf = font.render(self.question, True, white)
-        screen.blit(question_surf, (self.rect.x, self.rect.y - 20))
+        question_surf = FONT.render(self.question, True, WHITE)
+        SCREEN.blit(question_surf, (self.rect.x, self.rect.y - 20))
 
-        pygame.draw.rect(screen, self.color, self.rect, 5)
-        text_surface = font.render(self.text, True, white)
-        screen.blit(text_surface, (self.rect.x + 10, self.rect.y + 10))
+        pygame.draw.rect(SCREEN, self.color, self.rect, 5)
+        text_surface = FONT.render(self.text, True, WHITE)
+        SCREEN.blit(text_surface, (self.rect.x + 10, self.rect.y + 10))
 
-    def check_validity(self):
+    def check_validity(self) -> bool:
         """Checks if the input is valid according to the valid options list. Stripped and lowercase inputs also
         allowed. Returns True if input for self.text is valid and False if not valid."""
         if not self.text:
@@ -114,9 +123,10 @@ class Button:
     """Class for buttons to submit user's ideal laptop specs or go back to form.  Takes in the x and y positions,
     w (width), and h (height). Also has a specs attribute to store laptop specs from user input."""
 
-    def __init__(self, x, y, w, h, button_text):
+    def __init__(self, dim: tuple[int, int, int, int], button_text: str) -> None:
+        x, y, w, h = dim
         self.rect = pygame.Rect(x, y, w, h)
-        self.color = color_inactive
+        self.color = COLOR_INACTIVE
         self.specs = {}  # will include final data
         self.button_text = button_text
 
@@ -136,19 +146,19 @@ class Button:
 
     def draw_box(self):
         """Draws the button and text."""
-        pygame.draw.rect(screen, self.color, self.rect)
-        text_surface = font.render(self.button_text, True, white)
+        pygame.draw.rect(SCREEN, self.color, self.rect)
+        text_surface = FONT.render(self.button_text, True, WHITE)
         text_rect = text_surface.get_rect(center=self.rect.center)
-        screen.blit(text_surface, text_rect)
+        SCREEN.blit(text_surface, text_rect)
 
 
 class DisplayRecommendations:
     """A new screen to display final laptop recommendations."""
 
-    def __init__(self, recommendations):
+    def __init__(self, recommendations: dict):
         self.recommendations = recommendations
         self.scroll = 0
-        self.back_button = Button(1200, 75 - self.scroll, box_width, box_height, "Go to Form")
+        self.back_button = Button((1200, 75 - self.scroll, BOX_WIDTH, BOX_HEIGHT), "Go to Form")
         self.total_height = len(recommendations) * 150
 
     def display_recs(self, limit: int):
@@ -182,31 +192,31 @@ class DisplayRecommendations:
                     self.scroll -= event.y * scroll_speed
                     self.scroll = max(0, min(self.scroll, self.total_height - screen_height))
 
-            screen.fill(bg_color)
+            SCREEN.fill(BG_COLOR)
             title_font = pygame.font.Font(None, 30)
-            title_surf = title_font.render("Recommended Laptops", True, white)
-            screen.blit(title_surf, (650, 50 - self.scroll))
+            title_surf = title_font.render("Recommended Laptops", True, WHITE)
+            SCREEN.blit(title_surf, (650, 50 - self.scroll))
 
             primary_laptop = self.recommendations[0]
             primary_rect = pygame.Rect(100, 150 - self.scroll, 600, 400)
-            pygame.draw.rect(screen, color_inactive, primary_rect, 2)
+            pygame.draw.rect(SCREEN, COLOR_INACTIVE, primary_rect, 2)
 
-            name = font.render(f"Best Recommendation: {primary_laptop['Name']}", True, white)
-            screen.blit(name, (150, 200 - self.scroll))
+            name = FONT.render(f"Best Recommendation: {primary_laptop['Name']}", True, WHITE)
+            SCREEN.blit(name, (150, 200 - self.scroll))
 
             y_offset = 240
 
             for spec, value in primary_laptop.items():
                 if spec != 'Name' and spec != 'Image':
-                    spec_text = font.render(f"{spec}: {value}", True, white)
-                    screen.blit(spec_text, (150, y_offset - self.scroll))
+                    spec_text = FONT.render(f"{spec}: {value}", True, WHITE)
+                    SCREEN.blit(spec_text, (150, y_offset - self.scroll))
                     y_offset += 50
                 if spec == 'Image':
                     response = requests.get(value)
                     image_data = BytesIO(response.content)
                     img = pygame.image.load(image_data)
                     img = pygame.transform.scale(img, (300, 200))
-                    screen.blit(img, (350, 270 - self.scroll))
+                    SCREEN.blit(img, (350, 270 - self.scroll))
 
             for i in range(1, limit):
                 for _ in self.recommendations[i]:
@@ -216,17 +226,17 @@ class DisplayRecommendations:
                     x = x_start + col * (laptop_width + x_space)
                     y = y_start + row * (laptop_height + y_space) - self.scroll
                     if item_height < y < screen_height:
-                        pygame.draw.rect(screen, color_inactive, (x, y, laptop_width, laptop_height))
+                        pygame.draw.rect(SCREEN, COLOR_INACTIVE, (x, y, laptop_width, laptop_height))
                         if len(self.recommendations[i]['Name']) <= 40:
-                            name = font.render(f"{self.recommendations[i]['Name']}", True, white)
-                            screen.blit(name, (x + 20, y + 20))
+                            name = FONT.render(f"{self.recommendations[i]['Name']}", True, WHITE)
+                            SCREEN.blit(name, (x + 20, y + 20))
                         else:
                             rec_name_first_line = self.recommendations[i]['Name'][:40]
                             rec_name_second_line = self.recommendations[i]['Name'][40:]
-                            name_1 = font.render(f"{rec_name_first_line}", True, white)
-                            name_2 = font.render(f"{rec_name_second_line}", True, white)
-                            screen.blit(name_1, (x + 20, y + 20))
-                            screen.blit(name_2, (x + 20, y + 40))
+                            name_1 = FONT.render(f"{rec_name_first_line}", True, WHITE)
+                            name_2 = FONT.render(f"{rec_name_second_line}", True, WHITE)
+                            SCREEN.blit(name_1, (x + 20, y + 20))
+                            SCREEN.blit(name_2, (x + 20, y + 40))
 
             self.back_button.draw_box()
 
@@ -247,25 +257,25 @@ def load_boxes():
 
     input_boxes = []
 
-    half = len(questions) // 2
-    if len(questions) % 2 != 0:
+    half = len(QUESTIONS) // 2
+    if len(QUESTIONS) % 2 != 0:
         half += 1
 
     for i in range(half):
         x = 100
-        y = 100 + i * box_spacing
-        input_boxes.append(InputBox(x, y, box_width, box_height, questions[i], valid_options[i]))
+        y = 100 + i * BOX_SPACING
+        input_boxes.append(InputBox((x, y, BOX_WIDTH, BOX_HEIGHT), QUESTIONS[i], VALID_OPTIONS[i]))
 
-    for i in range(half, len(questions)):
+    for i in range(half, len(QUESTIONS)):
         x = 800
-        y = 100 + (i - half) * box_spacing
-        input_boxes.append(InputBox(x, y, box_width, box_height, questions[i], valid_options[i]))
+        y = 100 + (i - half) * BOX_SPACING
+        input_boxes.append(InputBox((x, y, BOX_WIDTH, BOX_HEIGHT), QUESTIONS[i], VALID_OPTIONS[i]))
 
-    submit_button = Button(800, 700, box_width, box_height, "Submit")
-    exit_button = Button(1050, 700, box_width, box_height, "Exit")
+    submit_button = Button((800, 700, BOX_WIDTH, BOX_HEIGHT), "Submit")
+    exit_button = Button((1050, 700, BOX_WIDTH, BOX_HEIGHT), "Exit")
 
     # todo: addition
-    sample_specs = Button(150, 700, box_width, box_height, "Sample")
+    sample_specs = Button((150, 700, BOX_WIDTH, BOX_HEIGHT), "Sample")
 
     run = True
     user_specs = None
@@ -296,7 +306,7 @@ def load_boxes():
                 max_price = randint(min_price, 1500)
                 data_spec_sample = [str(min_price), str(max_price)]
 
-                for lst in valid_options:
+                for lst in VALID_OPTIONS:
                     if lst is None:
                         continue
                     data_spec_sample.append(lst[randint(0, len(lst) - 1)])
@@ -327,9 +337,8 @@ def load_boxes():
                     filled += 1
 
                 # todo: attempt partial matching
-                enable_partial_matching = (filled > 2 and
-                                           all(b.text != '' for b in input_boxes[0:2]) and
-                                           input_boxes[8].text != '')
+                enable_partial_matching = (filled > 2 and all(b.text != '' for b in input_boxes[0:2])
+                                           and input_boxes[8].text != '')
                 # note that price values will be checked in previous try-except
 
                 empty_boxes = []
@@ -344,9 +353,9 @@ def load_boxes():
                             empty_boxes.append(data_q[i])
                             continue
 
-                        if valid_options[i] is None:
+                        if VALID_OPTIONS[i] is None:
                             user_specs[i] = float(box.text)
-                        elif isinstance(valid_options[i], tuple):
+                        elif isinstance(VALID_OPTIONS[i], tuple):
                             user_specs[i] = float(box.text)
                         else:
                             user_specs[i] = box.text
@@ -375,7 +384,7 @@ def load_boxes():
                 else:
                     show_error = True
 
-            form_screen.fill(bg_color)
+            form_screen.fill(BG_COLOR)
             for box in input_boxes:
                 box.draw_box()
             submit_button.draw_box()
@@ -385,23 +394,23 @@ def load_boxes():
             sample_specs.draw_box()
 
             # todo: HOW TO
-            how_to = font.render("Enter a proper value for the required fields marked (*)", True,
+            how_to = FONT.render("Enter a proper value for the required fields marked (*)", True,
                                  (255, 255, 255))
             form_screen.blit(how_to, (150, 800))
 
             if error_message and show_error:
-                error_surf = font.render(error_message, True, (255, 0, 0))  # Red for errors
+                error_surf = FONT.render(error_message, True, (255, 0, 0))  # Red for errors
                 form_screen.blit(error_surf, (800, 750))
 
             pygame.display.flip()
-            clock.tick(60)
+            CLOCK.tick(60)
 
     pygame.quit()
     return user_specs
 
 
 if __name__ == "__main__":
-    specs = load_boxes()
+    # specs = load_boxes()
 
     doctest.testmod()
     python_ta.check_all(config={
@@ -416,4 +425,4 @@ if __name__ == "__main__":
         'max-line-length': 120
     })
 
-    contracts.check_all_contracts("user_input_form.py")
+    python_ta.contracts.check_all_contracts("user_input_form.py")
